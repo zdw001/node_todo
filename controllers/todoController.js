4027437bec96d17962a9c9b185e47f7f9f2c359e
Zach Winters <zwintersdeveloper@gmail.com>
2017-10-19 15:38:35 -0400

    mongodb added

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,4 +1,20 @@
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+// Connect to the database
+// mongoose.Promise = global.Promise;﻿
+mongoose.connect('mongodb://test:test@ds125565.mlab.com:25565/todo', { useMongoClient: true });
+
+// Create a schema - this is like a blueprint
+var todoSchema = new mongoose.Schema({
+	item: String
+});
+
+var Todo = mongoose.model('Todo', todoSchema);
+var itemOne = Todo({item: 'get flowers'}).save(function(err){
+	if(err) throw err;
+	console.log('item saved');
+});
 
 var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'kick ass'}];
 var urlencodedParser = bodyParser.urlencoded({extended: false});
